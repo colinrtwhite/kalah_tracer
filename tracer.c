9dc09986d5fbf74367fa4077d36caeb7658cfec6
Colin White <colinrtwhite@gmail.com>
2014-11-18 23:59:11 +0000

    Fixed memory leak
    The hash list is now occasionally purged to a file so we don’t use more
    than approx. 1.6GB of memory at one time.

--- tracer.c
+++ tracer.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include "uthash.h"
 
-struct hash_move {
+struct hash_item {
 	char state[41]; // Key (string is WITHIN the structure)
 	int move;
 	int result;
@@ -11,7 +11,8 @@ struct hash_move {
 	UT_hash_handle hh; // Makes this structure hashable
 };
 
-#define MAX_DEPTH 50
+#define MAX_DEPTH 40
+#define MAX_HASHES 15000000
 #define SOUTH 0
 #define NORTH 1
 
@@ -20,14 +21,47 @@ struct hash_move {
 // 8 to 14 are NORTH's wells
 // 15 is NORTH's kalah
 int board_states[MAX_DEPTH][16];
-struct hash_move *best_moves = NULL;
+struct hash_item *game_tree = NULL;
 char board[41];
+int first_move, num_files;
 
 void convert_board_to_string(int depth, char *destination) {
 	for (int i = 0; i < 15; i++) {
-		destination += sprintf(destination, "%d-", board_states[depth][i]);
+		destination += sprintf(destination, "%d|", board_states[depth][i]);
 	}
 	destination += sprintf(destination, "%d", board_states[depth][15]);
+}
+
+int sort_hashes(struct hash_item *a, struct hash_item *b) {
+	if (a->depth < b->depth) {
+		return -1;
+	} else if (a->depth == b->depth) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+// Sort and write all the collected board states to a file
+// and clear the hash table
+void write_to_file() {
+	fprintf(stderr, "Beginning sort and write to file process %d.", num_files);
+	// Sort the hashes
+	HASH_SORT(game_tree, sort_hashes);
+
+	sprintf(board, "board_states_for_%d_num_%d.txt", first_move, num_files);
+	FILE *fp = fopen(board, "w+");
+	struct hash_item *i, *tmp;
+
+	HASH_ITER(hh, game_tree, i, tmp)
+	{
+		fprintf(fp, "%d~%s~%d~%d\n", i->depth, i->state, i->move, i->result);
+		HASH_DEL(game_tree, i);
+		free(i);
+	}
+
+	fclose(fp);
+	num_files++;
 }
 
 int check_game_over(int depth) {
@@ -44,7 +78,7 @@ int check_game_over(int depth) {
 
 // Calculate the best move and eventual outcome for the
 // board state in board_states[depth]
-int trace(int depth, int player, int start_move, int end_move) {
+int trace(int depth, int player) {
 	// Check if the game is won/lost/tied in this board state
 	int game_status = check_game_over(depth);
 	if (game_status != -99) {
@@ -52,9 +86,9 @@ int trace(int depth, int player, int start_move, int end_move) {
 	}
 
 	// Check if we've already encountered this board state
-	struct hash_move *entry;
+	struct hash_item *entry;
 	convert_board_to_string(depth, board);
-	HASH_FIND_STR(best_moves, board, entry);
+	HASH_FIND_STR(game_tree, board, entry);
 	if (entry) {
 		// This board state is identical to a previous point in the game tree,
 		// which has already been explored
@@ -66,15 +100,14 @@ int trace(int depth, int player, int start_move, int end_move) {
 	int opposite_player = (player + 1) % 2;
 	int opposite_player_offset = opposite_player * 8;
 	int opposite_player_kalah = opposite_player_offset + 7;
-	int best_move = -1, best_result = -2, subtree_best_result;
-	if (player == NORTH) {
-		best_result = 2;
-	}
+	int best_result = (player == SOUTH) ? -2 : 2;
+	int best_move = -1, subtree_best_result, beads;
 
 	// Calculate the end result of performing each possible move on the board state
-	for (int move = player_offset + start_move; move < player_kalah - (6 - end_move); move++) {
-		int beads = board_states[depth][move];
-		if (beads == 0) { // Skip empty wells
+	for (int move = player_offset; move < player_kalah; move++) {
+		beads = board_states[depth][move];
+		if (beads == 0 || (depth == 0 && move != first_move)) {
+			// Skip empty wells and only perform first_move at depth = 0
 			continue;
 		}
 
@@ -104,9 +137,9 @@ int trace(int depth, int player, int start_move, int end_move) {
 		}
 
 		if (end_point == player_kalah && depth != 0) { // Free turn (not on first turn)
-			subtree_best_result = trace(depth + 1, player, 0, 6);
+			subtree_best_result = trace(depth + 1, player);
 		} else {
-			subtree_best_result = trace(depth + 1, opposite_player, 0, 6);
+			subtree_best_result = trace(depth + 1, opposite_player);
 		}
 
 		if (player == SOUTH) {
@@ -135,66 +168,35 @@ int trace(int depth, int player, int start_move, int end_move) {
 		}
 		best_result = check_game_over(depth); // Should not return -99 at this point
 	} else if (player == SOUTH && best_result > -1) { // Don't hash losing board states to save space
-	// Hash the best move and win percentage with respect to board layout
-		struct hash_move *best_move_hash = (struct hash_move*) malloc(
-				sizeof(struct hash_move));
+		struct hash_item *best_move_hash = (struct hash_item*) malloc(
+				sizeof(struct hash_item));
 		convert_board_to_string(depth, best_move_hash->state);
 		best_move_hash->move = best_move;
 		best_move_hash->result = best_result;
 		best_move_hash->depth = depth; // Hash depth to help sort board states
-		HASH_ADD_STR(best_moves, state, best_move_hash);
+		// Hash the best move and win percentage with respect to board layout
+		HASH_ADD_STR(game_tree, state, best_move_hash);
+		if (HASH_COUNT(game_tree) == MAX_HASHES) { // Clear the memory
+			write_to_file();
+		}
 	}
 
 	return best_result;
 }
 
-int sort_hashes(struct hash_move *a, struct hash_move *b) {
-	if (a->depth < b->depth) {
-		return -1;
-	} else if (a->depth == b->depth) {
-		return 0;
-	} else {
-		return 1;
-	}
-}
-
-// Sort and write all the collected board states to a file
-void write_to_file(int start_move, int end_move) {
-	// Sort the hashes
-	HASH_SORT(best_moves, sort_hashes);
-
-	struct hash_move *i;
-	sprintf(board, "board_states_%d_to_%d.txt", start_move, end_move);
-	FILE *fp = fopen(board, "w+");
-
-	for (i = best_moves; i != NULL; i = i->hh.next) {
-		fprintf(fp, "%d#%s#%d#%d\n", i->depth, i->state, i->move, i->result);
-	}
-
-	fclose(fp);
-}
-
 int main() {
 	// Initialise the starting board
 	for (int i = 0; i < 16; i++) {
-		if (i == 7 || i == 15) {
-			board_states[0][i] = 0;
-		} else {
-			board_states[0][i] = 7;
-		}
+		board_states[0][i] = (i == 7 || i == 15) ? 0 : 7;
 	}
 
-	int start_move, end_move;
-	printf("Enter the minimum first move choice at depth 0: ");
-	scanf("%d", &start_move);
-	printf("Enter the maximum first move choice at depth 0: ");
-	scanf("%d", &end_move);
+	printf("Enter the first move choice (0 - 6) to divide up the game tree: ");
+	scanf("%d", &first_move);
 
 	// Should hopefully output 1 (meaning we are guaranteed to win as SOUTH)
-	printf("Best guaranteed result for SOUTH: %d\n", trace(0, SOUTH, start_move, end_move));
+	printf("Best guaranteed result for SOUTH: %d\n", trace(0, SOUTH));
 
-	printf("Beginning sort and write to file process.");
-	write_to_file(start_move, end_move);
+	write_to_file();
 
 	return 0;
 }
